070f07dc02637eb1a95f09b76a634415761b28bc
Konrad <szariii>
2021-12-27 23:09:34 +0100

    przesylka

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku! - kolejna mała zmiana pliku</h1>")
+    res.send("<h1>my app on heroku! - kolejna mała zmiana pliku - Konrad Szarek 3I1 gr1</h1>")
 })
 
 app.listen(PORT, function () {
